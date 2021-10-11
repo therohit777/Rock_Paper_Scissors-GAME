5fba4ae03081fdc82f298b61e378c65c136cb1ae
Priyanshu Sharma <priyanshusharma72002@gmail.com>
2021-10-11 13:31:57 +0530

    Play rock paper scissors with more ease

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -10,52 +10,72 @@ while(ask=="y" or ask=="y"):
     print("1. ROCK")
     print("2. PAPER")
     print("3. SCISSORS")
+    print("If any other number is choosed accept the above mentioned the round is terminated and the Player has to play the same round once again with the valid input!!")
     round=0
     while(round<5):
         c=random.randint(1,3)
         d=int(input("Enter your Material serial No. that is Above: "))
+        if(d>3 or d<1):
+         print("Your choice is not valid , please try again and the same round is played once again")
+         continue
         if(c==1):
+            print("Computer choosed ROCK")
             if(d==1):
+                print("You choosed ROCK")
                 l[round]="DRAW"
                 print("This round is a DRAW")
             elif(d==2):
+                print("You choosed PAPER")
                 l[round]="PLAYER"
                 b+=1
-                print("PLAYER won this Round")
+                print(" PLAYER won this Round")
             elif(d==3):
+                print("You choosed Scissors")
                 l[round]="COMPUTER"
                 a+=1
                 print("COMPUTER won this Round")
+          
+                   
         elif(c==2):
+            print("Computer choosed PAPER")
             if(d==1):
+                print("You choosed ROCK")
                 l[round]="COMPUTER"
                 a+=1
                 print("COMPUTER won this Round")
             elif(d==2):
+                print("You choosed PAPER")
                 l[round]="DRAW"
                 print("This round is a DRAW")
             elif(d==3):
+                print("You choosed Scissors")
                 l[round]="PLAYER"
                 b+=1
                 print("PLAYER won this Round")
         elif(c==3):
+            print("Computer choosed SCISSORS")
             if(d==1):
+                print("You choosed ROCK")
                 l[round]="PLAYER"
                 b+=1
                 print("PLAYER won this Round")
             elif(d==2):
+                print("You choosed PAPER")
                 l[round]="COMPUTER"
                 a+=1
                 print("COMPUTER won this Round")
             elif(d==3):
+                print("You choosed Scissors")
                 l[round]="DRAW"
                 print("This round is a DRAW")  
         round+=1 
-    print(round)
+    print("Five rounds completed")
     print("-------------------------RESULTS OF THE MATCH-------------------------")
     print("ROUNDWISE WINNER")
     for i in l:
         print(i+1,l[i])
+    print("Player's Score is:",b)    
+    print("Computer's Score is:",a)
     if(a>b):
         print("COMPUTER IS THE WINNER.")
     elif(b>a):
